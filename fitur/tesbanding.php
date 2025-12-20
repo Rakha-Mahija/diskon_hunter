163729aac4fac81da0b911e957017bb169f1dd4e
Rakha-Mahija <24523103@students.uii.ac.id>
2025-12-20 12:44:05 +0700

    fungsi update tabel dan insert history

--- fitur/tesbanding.php
+++ fitur/tesbanding.php
@@ -6,6 +6,35 @@ $result = $db->query("
     SELECT id_scrap, id_user, link, harga, diskon 
     FROM scrap
 ");
+function updateScrap($db, $id_scrap, $harga_baru, $diskon_baru) {
+    $stmt = $db->prepare("
+        UPDATE scrap 
+        SET harga = ?, diskon = ?, scrap_at = NOW()
+        WHERE id_scrap = ?
+    ");
+    $stmt->bind_param("iii", $harga_baru, $diskon_baru, $id_scrap);
+    $stmt->execute();
+    $stmt->close();
+}
+
+function insertHistory($db, $id_scrap, $harga_lama, $diskon_lama, $harga_baru, $diskon_baru) {
+    $stmt = $db->prepare("
+        INSERT INTO history 
+        (id_scraps, last_harga, last_diskon, new_harga, new_diskon, checked_at)
+        VALUES (?, ?, ?, ?, ?, NOW())
+    ");
+    $stmt->bind_param(
+        "iiiii",
+        $id_scrap,
+        $harga_lama,
+        $diskon_lama,
+        $harga_baru,
+        $diskon_baru
+    );
+    $stmt->execute();
+    $stmt->close();
+}
+
 
 while ($row = $result->fetch_assoc()) {
 
@@ -49,13 +78,13 @@ while ($row = $result->fetch_assoc()) {
     $harga_baru  = $data['harga'];
     $diskon_baru = $data['diskon'];
 
-    // 🔥 PERBANDINGAN
-    if ($harga_baru != $harga_lama || $diskon_baru != $diskon_lama) {
-
-        echo "ada perubahan";
-        echo "<br>";
+   
+    if ($harga_baru < $harga_lama || $diskon_baru > $diskon_lama) {
+    insertHistory($db,$id_scrap,$harga_lama,$diskon_lama,$harga_baru,$diskon_baru);
+    updateScrap($db,$id_scrap,$harga_baru,$diskon_baru);
+    echo "ada diskon<br>";
     }else{
-        echo "tidak ada perubahan";
+        echo "tidak ada perubahan<br>";
     }
 }
 }
